725ba7185e1149af25d6e0c2420530d3168e9951
root <root@ip-172-31-31-76.ec2.internal>
2021-05-17 09:40:27 +0000

    Creado 3php

---
+++ Estructuras_de_control/3.php
@@ -0,0 +1,31 @@
+<?php
+/* 3. Escribe un script PHP que genere un número aleatorio entre 1 y 7,
+y muestre un mensaje indicando a qué día de la semana corresponde.
+Por ejemplo, 1 sería lunes, 2 martes, etc.*/
+$dia = rand (1,7);
+echo "El número $dia corresponde al";
+echo "<br>";
+switch ($dia){
+    case 1:
+        echo "Lunes";
+        break;
+    case 2:
+        echo "Martes";
+        break;
+    case 3:
+        echo "Miércoles";
+        break;
+    case 4:
+        echo "Jueves";
+        break;
+    case 5:
+        echo "Viernes";
+        break;
+    case 6:
+        echo "Sábado";
+        break;
+    case 7:
+        echo "Domingo";
+        break;
+}
+?>
