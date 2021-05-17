1a0b556431182d2be1cb998564cb7efdcc75167a
root <root@ip-172-31-31-76.ec2.internal>
2021-05-17 09:47:58 +0000

    Creado 6 php

---
+++ Estructuras_de_control/6.php
@@ -0,0 +1,9 @@
+<?php
+/* Escriba un script PHP que realice la simulación de lanzar un dado
+y muestre una imagen con un valor aleatorio enre 1 y 6.
+Resuelva el ejercicio sin utilizar las estructuras de control if - else y switch.*/
+
+$variable = rand(1,6);
+echo "<h1>Tu tirada: $variable</h1>";
+echo "<img src='images/dados/$variable.png'>";
+?>
