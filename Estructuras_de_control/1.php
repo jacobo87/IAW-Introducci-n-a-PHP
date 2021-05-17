4b68967e1b9bdecd8715b870acf237b4d877b45f
root <root@ip-172-31-31-76.ec2.internal>
2021-05-17 09:51:02 +0000

    Corregido 1 php

--- Estructuras_de_control/1.php
+++ Estructuras_de_control/1.php
@@ -2,8 +2,8 @@
 /* Escribe un script que simule el comportamiento de lanzar una moneda al aire y muestre una imagen con la cara o la cruz de la moneda. */
 $moneda = rand (0,1);
 if ($moneda == 1) {
-    echo "<img src='IAW-Introduccion-a-PHP/images/moneda1.png'>";
+    echo "<img src='../images/moneda1.png'>";
 } else {
-    echo "<img src='IAW-Introduccion-a-PHP/images/moneda2.png'>";
+    echo "<img src='../images/moneda2.png'>";
 }
 ?>
